925cdcab5c46461a2fe600fa35c91a023c77e05e
Laura-C-M <shark01963@sharkcoders.pt>
2026-06-05 19:32:05 +0100

    porjeto final

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+import random
+import sys
+import time
+from dataclasses import dataclass
+from typing import List, Optional, Tuple
+
+import cv2
+import mediapipe as mp
+import numpy as np
+import pygame
